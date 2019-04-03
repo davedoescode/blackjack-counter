d4157868b796e61775e7dfda3165509e89b5013e
davedoescode <mddavies@ualberta.ca>
2019-04-03 17:06:08 -0600

    made figures better, added seaborn

--- statFunctions.py
+++ statFunctions.py
@@ -1,27 +1,36 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 def plotWinLoss(stats):
   handsPlayed = range(1, len(stats) + 1)
   chipDiff = []
   totalChips = []
+  colors = []
 
   for i in range(len(stats)):
     if stats[i][0] == 0:
       chipDiffWinOrLoss = stats[i][2] - stats[i-1][2]
+      colors.append('r')
     elif stats[i][0] == 1:
       chipDiffWinOrLoss = stats[i][2] - stats[i-1][2]
+      colors.append('g')
     elif stats[i][0] == -1:
       chipDiffWinOrLoss = 0
+      colors.append('b')
 
     chipDiff.append(chipDiffWinOrLoss)
     totalChips.append(stats[i][2])
 
+  sns.set()
+
   plt.subplot(2, 1, 1)
-  plt.plot(handsPlayed, totalChips, 'o-', color='green')
-  plt.title('Blackjack Statistics')
-  plt.ylabel('Chip Total')
+  plt.plot(handsPlayed, totalChips, 'o--', color='g')
+  plt.title('Blackjack Statistics', size=12)
+  plt.ylabel('Chip Total', size=10)
 
   plt.subplot(2, 1, 2)
-  plt.bar(handsPlayed, chipDiff, width=0.1, align='center', color='green')
+  plt.bar(handsPlayed, chipDiff, width=0.1, color=colors)
+  plt.ylabel('Chip Differential Per Hand', size=10)
+  plt.xlabel('Hands', size=10)
 
   plt.show()
